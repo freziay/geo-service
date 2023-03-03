15d8586f2ecf224461f92e3df2637fa2678b2581
Nastena <freziay@mail.ru>
2023-03-03 14:29:44 +0300

    "messageSenderMockitoСhanges"

--- src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -3,13 +3,19 @@ package ru.netology.i18n;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import ru.netology.entity.Country;
+
+import static ru.netology.entity.Country.RUSSIA;
+import static ru.netology.entity.Country.USA;
 
 class LocalizationServiceTest {
+    LocalizationService localizationService = new LocalizationServiceImpl();
+
 
     @ParameterizedTest
-    @ValueSource(strings = "Добро пожаловать")
+    @ValueSource(strings = {"Добро пожаловать"})
     void locale(String country) {
-        String salute = "Добро пожаловать";
-        Assertions.assertEquals(country, salute);
+        String countryTest = localizationService.locale(RUSSIA);
+        Assertions.assertEquals(country, countryTest);
     }
 }
